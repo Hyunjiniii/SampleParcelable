b76c1dae1f41a98a1686646098fbefc47840d900
hyunjin <dksguswls121@naver.com>
2019-04-14 19:04:33 +0900

    Parcelable Example

--- app/src/main/java/com/example/sampleparcelable/MenuActivity.java
+++ app/src/main/java/com/example/sampleparcelable/MenuActivity.java
@@ -22,22 +22,30 @@ public class MenuActivity extends AppCompatActivity {
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // 인텐트 객체 생성
                 Intent intent = new Intent();
                 intent.putExtra("name", "mike");
 
+                // 응답 전달 후 액티비티 종료
                 setResult(RESULT_OK, intent);
                 finish();
             }
         });
 
+        // 메인 액티비티로부터 전달받은 인텐트 확인
         Intent intent = getIntent();
         processIntent(intent);
     }
 
     private void processIntent(Intent intent) {
         if (intent != null) {
+            // 인텐트 안의 번들 객체 참조
             Bundle bundle = intent.getExtras();
+
+            // 번들 객체 안의 SimpleData 객체 참조
             SimpleData data = (SimpleData) bundle.getParcelable(KEY_SIMPLE_DATA);
+
+            // 텍스트뷰에 값 설정
             textView.setText("전달받은 데이터\nNumber : " + data.getNum() + "\nMessage : " + data.getMessage());
         }
     }
